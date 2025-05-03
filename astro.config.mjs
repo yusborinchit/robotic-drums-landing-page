0307b86681be61f1eeaeb3f4dd3b72986e6eae0b
Santiago García <sgarciacedres@gmail.com>
2025-05-02 23:32:39 -0300

    feat: :wastebasket: remove boilerplate code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,11 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
 import compress from 'astro-compress'
-import icon from 'astro-icon'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  compressHTML: true,
-  site: 'https://accessible-astro.netlify.app',
   integrations: [
-    mdx(),
-    icon(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
+    tailwind(),
     compress(),
   ],
-  vite: {
-    css: {
-      preprocessorOptions: {
-        scss: {
-          logger: {
-            warn: () => {},
-          },
-        },
-      },
-    },
-  },
 })
