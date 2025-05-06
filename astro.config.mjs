3e474ae0d5c572aa44c4b07bb4ab82b76322cf14
Santiago García <sgarciacedres@gmail.com>
2025-05-06 14:39:16 -0300

    feat: :sparkles: add icons with astro icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
-import compress from 'astro-compress'
-import { defineConfig } from 'astro/config'
-
 import tailwindcss from '@tailwindcss/vite';
+import compress from 'astro-compress';
+import icon from 'astro-icon';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    compress(),
-  ],
-
+  integrations: [compress(), icon()],
   vite: {
     plugins: [tailwindcss()],
   },
