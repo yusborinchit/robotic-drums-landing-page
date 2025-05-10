3419eb6ca62e9b8adcf8ca5202c55b1fd3b7e21a
Santiago García <sgarciacedres@gmail.com>
2025-05-10 00:17:00 -0300

    feat: :sparkles: add animations with motion

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import compress from 'astro-compress';
 import icon from 'astro-icon';
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compress(), icon()],
+  integrations: [compress(), icon(), react()],
   vite: {
     plugins: [tailwindcss()],
   },
